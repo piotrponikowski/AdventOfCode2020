aa2d3b019eba343017718b429370224d7922ce3a
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2020-12-14 20:41:37 +0100

    day 13 cleanup

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -4,23 +4,16 @@ class Day13(input: List<String>) {
     val buses = input.last().split(",")
         .mapIndexedNotNull { index, data -> data.takeIf { it != "x" }?.let { Bus(index, data.toLong()) } }
 
-    fun solve1() = buses.map { (index, id) -> id to id - (timestamp % id) }
-        .minByOrNull { it.second }!!
-        .let { it.first * it.second }
+    fun solve1() = buses.map { (_, id) -> id to id - (timestamp % id) }
+        .minByOrNull { (_, diff) -> diff }!!
+        .let { (index, diff) -> index * diff }
 
-    fun solve2(): Long {
-        var time = 0L
-        var step = buses.first().id
-
-        buses.drop(1).forEach { bus ->
-            while ((time + bus.index) % bus.id != 0L) {
-                time += step
-            }
-            step = lcm(step, bus.id)
-        }
-
-        return time
-    }
+    fun solve2() = buses.drop(1)
+        .fold(0L to buses.first().id) { (time, step), bus ->
+            generateSequence(time) { it + step }
+                .first { (it + bus.index) % bus.id == 0L }
+                .let { it to step * bus.id }
+        }.let { (time, step) -> time }
 
     data class Bus(val index: Int, val id: Long)
 }
