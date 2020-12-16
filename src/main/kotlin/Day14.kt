74f38fb810cf1c19ed33aa952a40312d903a362c
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2020-12-16 16:29:41 +0100

    day 14 backup

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,66 +1,43 @@
-import java.lang.System.lineSeparator
+class Day14(input: List<String>) {
 
-class Day14(input: String) {
-
-    val pattern = Regex("([0-9]+).*?([0-9]+)")
-
-    val groups = input.split("mask = ")
-        .filter { it.isNotBlank() }
-        .map { it.split(lineSeparator()) }
-        .map { group ->
-            Group(group.first(), group.drop(1).filter { it.isNotBlank() }.map { pattern.find(it)!!.destructured }
-                .map { (memory, value) -> Instruction(memory.toLong(), value.toLong()) })
-        }
+    val instructions = parse(input)
 
     fun solve1(): Long {
         val memory = mutableMapOf<Long, Long>()
+        var enabledMask: String
+        var disabledMask: String
 
-        groups.forEach { group ->
-            group.instructions.forEach { instruction ->
-                memory[instruction.address] = (instruction.value and group.maskOn()) or group.maskOff()
+
+
+        instructions.forEach { instruction ->
+            when (instruction) {
+                is Instruction.SetMask -> println("test")
+                is Instruction.SetValue -> println("test")
             }
         }
-
-        println(memory)
 
         return memory.values.sum()
     }
 
-    fun solve2(): Long {
-        val memory = mutableMapOf<Long, Long>()
 
-        groups.take(1).forEach { group ->
-            val addresses = group.options().map { it.toLong(2) }
+    companion object {
+        private val PATTERN_MASK = Regex("[X0-9]+")
+        private val PATTERN_VALUE = Regex("([0-9]+).*?([0-9]+)")
 
-            group.instructions.forEach { instruction ->
-                addresses.forEach { address ->
-                    println(address.toString(2).padStart(32, '0'))
-                    println(instruction.address.toString(2).padStart(32, '0'))
-                    println(group.maskOn().toString(2).padStart(32, '0'))
-                    println(group.maskOff().toString(2).padStart(32, '0'))
-println()
-                    address.toULong().inv()
-
-                    val test = (instruction.value or group.maskOn())
-
-                    memory[address or test] = instruction.value
-                }
-            }
-        }
-
-        println(memory)
-        return memory.values.sum()
-    }
-
-
-    data class Group(val mask: String, val instructions: List<Instruction>) {
-        fun maskOff() = mask.replace("X", "0").toLong(2)
-        fun maskOn() = mask.replace("X", "1").toLong(2)
-        fun options(next: String = mask): List<String> {
-            return if (!next.contains("X")) listOf(next)
-            else options(next.replaceFirst("X", "1")) + options(next.replaceFirst("X", "0"))
+        fun parse(input: List<String>) = input.map {
+            if (it.contains("mask")) PATTERN_MASK.find(it)!!.groupValues
+                .let { (mask) -> Instruction.SetMask(mask) }
+            else PATTERN_VALUE.find(it)!!.groupValues
+                .let { (addres, value) -> Instruction.SetValue(addres, value.toLong()) }
         }
     }
 
-    data class Instruction(val address: Long, val value: Long)
+    sealed class Instruction {
+        data class SetMask(val mask: String) : Instruction() {
+            fun enabled() = mask.replace('X', '1')
+            fun disabled() = mask.replace('X', '0')
+        }
+
+        data class SetValue(val address: String, val value: Long) : Instruction()
+    }
 }
