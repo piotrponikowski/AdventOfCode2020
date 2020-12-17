28bb1d56e6c8edcab836c8bb1b151ee9b1388fdf
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2020-12-17 09:21:00 +0100

    day 17 part 2

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -1,17 +1,20 @@
 class Day17(input: List<String>) {
 
-    val startingPoints = input
+     val startingPoints = input
         .flatMapIndexed { y, row ->
             row.mapIndexedNotNull { x, cell ->
-                if (cell == '#') Point(x, y, 0) else null
+                if (cell == '#') Point(x, y, 0, 0) else null
             }
         }.toSet()
 
-    val neighbours =
+     val neighbours =
         (-1..1).flatMap { dx ->
             (-1..1).flatMap { dy ->
-                (-1..1).mapNotNull { dz ->
-                    if (!(dx == 0 && dy == 0 && dz == 0)) Point(dx, dy, dz) else null
+                (-1..1).flatMap { dz ->
+                    (-1..1).mapNotNull { dw ->
+                        if (!(dx == 0 && dy == 0 && dz == 0 && dw == 0)) Point(dx, dy, dz, dw) else null
+
+                    }
                 }
             }
         }.toSet()
@@ -37,19 +40,25 @@ class Day17(input: List<String>) {
         val zMin = prevPoints.minOf { it.z } - 1
         val zMax = prevPoints.maxOf { it.z } + 1
 
+        val wMin = prevPoints.minOf { it.w } - 1
+        val wMax = prevPoints.maxOf { it.w } + 1
+
         (xMin..xMax).forEach { x ->
             (yMin..yMax).forEach { y ->
                 (zMin..zMax).forEach { z ->
-                    val nextPoint = Point(x, y, z)
-                    val isActive = prevPoints.contains(nextPoint)
-                    val activeNeighbours = neighbours.count { neighbour -> prevPoints.contains(nextPoint + neighbour) }
+                    (wMin..wMax).forEach { w ->
+                        val nextPoint = Point(x, y, z, w)
+                        val isActive = prevPoints.contains(nextPoint)
+                        val activeNeighbours =
+                            neighbours.count { neighbour -> prevPoints.contains(nextPoint + neighbour) }
 
-                    if (isActive && activeNeighbours in 2..3) {
-                        nextPoints += nextPoint
-                    }
+                        if (isActive && activeNeighbours in 2..3) {
+                            nextPoints += nextPoint
+                        }
 
-                    if (!isActive && activeNeighbours == 3) {
-                        nextPoints += nextPoint
+                        if (!isActive && activeNeighbours == 3) {
+                            nextPoints += nextPoint
+                        }
                     }
                 }
             }
@@ -59,13 +68,13 @@ class Day17(input: List<String>) {
     }
 
 
-    data class Point(val x: Int, val y: Int, val z: Int) {
-        operator fun plus(other: Point) = Point(this.x + other.x, this.y + other.y, this.z + other.z)
+    data class Point(val x: Int, val y: Int, val z: Int, val w: Int) {
+        operator fun plus(p: Point) = Point(this.x + p.x, this.y + p.y, this.z + p.z, this.w + p.w)
     }
 }
 
 fun main() {
-    val input = Utils.readLines("day17.txt", true)
+    val input = Utils.readLines("day17.txt")
     val day = Day17(input)
     day.solve1()
 
