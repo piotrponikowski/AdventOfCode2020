d82258181f88bd5896ca33173bd91b8bf7e9e56f
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2020-12-19 14:32:27 +0100

    day 18 part 2 refactor

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -1,74 +1,58 @@
-class Day19(input: List<String>) {
+import java.lang.System.lineSeparator
+
+class Day19(input: String) {
 
     val messages = parseMessages(input)
-    val rules = parseRules(input)
-    val refs = parseRefs(input)
+    val rules1 = parseRules(input)
+    val rules2 = rules1 + listOf(8 to "42 | 42 8", 11 to "42 31 | 42 11 31")
 
-    fun solve1(): Int {
-        solveRules()
-        return messages.count { it in rules[0]!!.values }
-    }
+    fun solve1() = messages.count { message -> matchRules(message, listOf(0)) }
 
-    fun solveRules() {
+    fun solve2() = messages.count { message -> matchRules(message, listOf(0)) }
 
-        while (refs.isNotEmpty()) {
-            val canBeSolved = refs.values.filter { ref -> ref.logics.all { logic -> rules.keys.containsAll(logic) } }
-            rules += canBeSolved.map { ref ->
-                val values = ref.logics.flatMap { logic ->
-                    logic.fold(listOf("")) { result, id -> combine(result, rules[id]!!.values) }
+
+    fun matchRules(message: String, messageRules: List<Int>): Boolean {
+//        println("#### $message, rules: $messageRules")
+        if (message.isEmpty()) {
+            return messageRules.isEmpty()
+        } else if (messageRules.isEmpty()) {
+            return false
+        }
+
+        rules2.getValue(messageRules.first()).let { logic ->
+            if (logic[1] in 'a'..'b') {
+                return if (message.startsWith(logic[1])) {
+//                    println("matching: $message rules: $rules")
+                    matchRules(message.drop(1), messageRules.drop(1))
+                } else {
+                    false
                 }
-                ref.id to Rule(ref.id, values)
-            }
-
-            refs -= canBeSolved.map { it.id }
-//            println(canBeSolved)
-        }
-    }
-
-//    fun refToValues(logic: List<Int>, result: List<String> = listOf("")): List<String> {
-//        if (logic.isEmpty()) return result
-//        else refToValues(logic.drop(1), result.map { })
-//    }
-
-    fun combine(list1: List<String>, list2: List<String>): List<String> {
-        val result = mutableListOf<String>()
-        list1.forEach { element1 ->
-            list2.forEach { element2 ->
-                result += element1 + element2
+            } else {
+                return logic.split(" | ").any { nextRules ->
+                    matchRules(message, nextRules.split(" ").map { it.toInt() } + messageRules.drop(1))
+                }
             }
         }
-        return result
     }
 
     companion object {
-        private val PATTERN_MESSAGE = Regex("[ab]+")
-        private val PATTERN_RULE = Regex("([0-9]+): \"([ab]+)\"")
-        private val PATTERN_REF = Regex("([0-9]+): ([0-9 |]+)")
 
-        fun parseMessages(input: List<String>) = input.filter { it.matches(PATTERN_MESSAGE) }
+        fun parseRules(input: String) = splitInput(input, 0)
+            .map { line -> line.split(":") }
+            .map { (id, logic) -> id.toInt() to logic.trim() }
+            .toMap()
 
-        fun parseRules(input: List<String>) = input.filter { it.matches(PATTERN_RULE) }
-            .map { PATTERN_RULE.matchEntire(it)!!.destructured }
-            .map { (id, value) -> id.toInt() to Rule(id.toInt(), listOf(value)) }
-            .toMap().toMutableMap()
+        fun parseMessages(input: String) = splitInput(input, 1)
 
-        fun parseRefs(input: List<String>) = input.filter { it.matches(PATTERN_REF) }
-            .map { PATTERN_REF.matchEntire(it)!!.destructured }
-            .map { (id, logic) -> id.toInt() to logic.split(" | ").map { it.split(" ").map { it.toInt() } } }
-            .map { (id, refs) -> id to Ref(id, refs) }
-            .toMap().toMutableMap()
+        private fun splitInput(input: String, group: Int) = input.split(lineSeparator().repeat(2))[group]
+            .split(lineSeparator())
     }
-
-    data class Rule(val id: Int, val values: List<String>)
-    data class Ref(val id: Int, val logics: List<List<Int>>)
 }
 
 fun main() {
-    val input = Utils.readLines("day19.txt")
+    val input = Utils.readText("day19.txt")
     val day = Day19(input).solve1()
 
-
-//    val result = Day19(input).combine(listOf("a", "b"), listOf("c", "d", "e"))
 
     println(day)
 
