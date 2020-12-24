73d99a7d3677fbfad5526d493fba2818f6e7438f
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2020-12-24 09:16:00 +0100

    day 24 part 2

--- src/main/kotlin/Day24.kt
+++ src/main/kotlin/Day24.kt
@@ -7,7 +7,35 @@ class Day24(input: List<String>) {
         "w" to Point(-2, 0), "sw" to Point(-1, 1), "nw" to Point(-1, -1),
     )
 
-    fun solve1(): Int {
+    fun solve1() = solveInitialMaze().count()
+
+    fun solve2(): Int {
+        var maze = solveInitialMaze()
+        repeat(100) {
+            val newMaze = mutableSetOf<Point>()
+
+            maze.forEach { blackTile ->
+                val tilesToCheck = neighbours.values.map { neighbour -> blackTile + neighbour } + blackTile
+                tilesToCheck.forEach { tileToCheck ->
+
+                    val isBlack = maze.contains(tileToCheck)
+                    val blackNeighbours = neighbours.values
+                        .count { neighbour -> maze.contains(tileToCheck + neighbour) }
+
+
+                    if ((isBlack && blackNeighbours in 1..2) || (!isBlack && blackNeighbours == 2)) {
+                        newMaze.add(tileToCheck)
+                    }
+                }
+            }
+
+            maze = newMaze
+        }
+
+        return maze.count()
+    }
+
+    private fun solveInitialMaze(): Set<Point> {
         val referenceTile = Point(0, 0)
         val blackTiles = mutableSetOf<Point>()
 
@@ -20,9 +48,9 @@ class Day24(input: List<String>) {
                 blackTiles.add(targetTile)
             }
         }
-
-        return blackTiles.count()
+        return blackTiles
     }
+
 
     companion object {
         private val PATTERN_DIRECTION = Regex("e|se|sw|w|nw|ne")
@@ -38,8 +66,6 @@ class Day24(input: List<String>) {
 fun main() {
     val input = Utils.readLines("day24.txt")
     val day = Day24(input)
-    val test = Day24.parseDirections("nwwswee")
-    println(test)
     println(day.solve1())
-
+    println(day.solve2())
 }
